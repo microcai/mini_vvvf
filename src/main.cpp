41455de65ccd91ae46b4244c9ed15b0028932d3c
microcai <microcaicai@gmail.com>
2022-10-31 18:05:20 +0800

    with current sensing and limiting

--- src/main.cpp
+++ src/main.cpp
@@ -4,24 +4,48 @@
 #include <esp_task_wdt.h>
 #include <esp_sleep.h>
 
+#define IOUTA 34
+#define IOUTB 35
+#define IOUTC 32
+
 // BLDC motor & driver instance
 // BLDCMotor motor = BLDCMotor(pole pair number);
-BLDCMotor motor = BLDCMotor(2);
+BLDCMotor motor = BLDCMotor(1);
 // BLDCDriver3PWM driver = BLDCDriver3PWM(pwmA, pwmB, pwmC, Enable(optional));
 BLDCDriver3PWM driver = BLDCDriver3PWM(25,26,27,33);
 
 MagneticSensorI2C sensor = MagneticSensorI2C(AS5600_I2C);
 
+LowsideCurrentSense current_sense = LowsideCurrentSense(0.005f, 10, IOUTA, IOUTB, IOUTC);
+
 float pi = 3.14159265358979;
 //target variable
 float target_hz = 10;
+
+float motor_volt = 220;
+float motor_freq = 60;
 
 // instantiate the commander
 Commander command = Commander(Serial);
 void doTarget(char* cmd)
 {
-  command.scalar(&target_hz, cmd);
-  //motor.voltage_limit = target_hz / 100 * driver.voltage_power_supply;
+  float req_hz = 3;
+  command.scalar(&req_hz, cmd);
+  if (req_hz >= 0)
+  {
+    if (req_hz < 3)
+      target_hz = 3;
+    else
+      target_hz = req_hz;
+  }
+  else if (req_hz > -3)
+  {
+    target_hz = -3;
+  }
+  else
+    target_hz = req_hz;
+  float volt_hz = std::abs(req_hz);
+  motor.voltage_limit = std::max(12.0f, std::min(driver.voltage_limit, volt_hz / motor_freq * motor_volt * 1.4f));
 }
 
 TaskHandle_t Task_idle;
@@ -33,8 +57,14 @@ static void Task_idle_code( void * pvParameters )
     vTaskDelay(10);
     command.run();
     vTaskDelay(10);
+    Serial.printf("current is %f\t %f\n", current_sense.getDCCurrent(), current_sense.getPhaseCurrents().a);
     esp_task_wdt_reset();
   }
+}
+
+static void printCurrent(char* cmd)
+{
+  Serial.printf("current is %f\n", current_sense.getPhaseCurrents().a);
 }
 
 void setup()
@@ -45,20 +75,27 @@ void setup()
 
   // driver config
   // power supply voltage [V]
-  driver.voltage_power_supply = 12;
+  driver.voltage_power_supply = 24;
   // limit the maximal dc voltage the driver can set
   // as a protection measure for the low-resistance motors
   // this value is fixed on startup
-  driver.voltage_limit = 6;
+  driver.voltage_limit = 24;
+  driver.pwm_frequency = 12000;
   driver.init();
+  current_sense.linkDriver(&driver);
+
+  current_sense.init();
+
   // link the motor and the driver
   motor.linkDriver(&driver);
+  motor.linkCurrentSense(&current_sense);
 
   // limiting motor movements
   // limit the voltage to be set to the motor
   // start very low for high resistance motors
   // currnet = resistance*voltage, so try to be well under 1Amp
-  motor.voltage_limit = 3;   // [V]
+  motor.voltage_limit = 24;   // [V]
+  motor.current_limit = 1; // [A]
 
   // open loop control config
   motor.controller = MotionControlType::velocity_openloop;
@@ -68,6 +105,7 @@ void setup()
 
   // add target command T
   command.add('T', doTarget, "target hz");
+  command.add('C', printCurrent, "print current");
 
   Serial.println("Motor ready!");
   Serial.println("Set target velocity [rad/s]");
